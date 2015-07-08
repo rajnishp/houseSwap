ed9b0440fc96f35610694bc7c624463d038bbbe4
rahul lahoria <rahullahoria@CAP-LAP-447.(none)>
2015-07-08 22:10:44 +0530

    add libs

--- src/dao/DAOFactory.class.php
+++ src/dao/DAOFactory.class.php
@@ -2,15 +2,27 @@
 
 /**
  * DAOFactory
- * @author: http://phpdao.com
- * @date: ${date}
+ * @author: rahul lahoria
+ * 
  */
+require_once('utils/sql/Connection.class.php');
+require_once('utils/sql/ConnectionFactory.class.php');
+require_once('utils/sql/ConnectionProperty.class.php');
+require_once('utils/sql/QueryExecutor.class.php');
+require_once('utils/sql/Transaction.class.php');
+require_once('utils/sql/SqlQuery.class.php');
+require_once('utils/ArrayList.class.php');
 class DAOFactory{
 	
 	/**
 	 * @return HousesDAO
 	 */
 	public static function getHousesDAO(){
+		require_once('HouseDAO.class.php');
+		require_once('models/House.class.php');
+		require_once('mysql/HouseMySqlDAO.class.php');
+		require_once('mysql/ext/HouseMySqlExtDAO.class.php');
+		
 		return new HousesMySqlExtDAO();
 	}
 
